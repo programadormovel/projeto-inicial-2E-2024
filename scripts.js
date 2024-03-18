6aaed43451c2d43d8a84d388cc3f418be2bdb3c9
programadormovel <programadormovel@gmail.com>
2024-03-18 09:56:07 -0300

    formulário

--- scripts.js
+++ scripts.js
@@ -1,7 +1,30 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
+var nome = document.getElementById("nome");
+
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
 
 function alertar(){
-    alert("Você clicou no botão!!!");
+    //alert(nome.value + " " + "clicou no botão!!!");
+    saida.innerText = "Nome: " + " " + nome.value +
+    "\n E-mail: "+ " " + email.value + 
+    "\n Telefone: "+ " "+ telefone.value + 
+    "\n CEP: "+ " " + cep.value + 
+    "\n Logradouro: "+ " " + logradouro.value + 
+    "\n Número: "+ " " + numero.value + 
+    "\n Complemento: "+ " " + complemento.value + 
+    "\n Bairro: "+ " " + bairro.value + 
+    "\n Cidade: "+ " " + cidade.value + 
+    "\n Estado: "+ " " + estado.value;
 }
-
+ 
